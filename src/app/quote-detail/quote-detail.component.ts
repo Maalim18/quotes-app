977070c0abb16938ac03f889241a1e7b4e814779
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-05-16 17:26:13 +0300

    add the changes

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component,EventEmitter,Input,Output, OnInit } from '@angular/core';
+import{Quote} from '../quote';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+  @Input() quoty: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+  deleteQuote(read:boolean){
+    this.isRead.emit(read);
+  }
+  upvote(){
+    this.quoty.upvote+=1;
+  }
+  downvote(){
+    this.quoty.downvote+=1;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import{Quote}from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1,'John', 'Yes we can!','Obama',20,10,new Date(2021,4,15)),
+    new Quote(1,'Mahatma Gandi', 'An eye for an eye only ends up making the whole world blind','Obama',20,10,new Date(2021,4,15)),
      new Quote(2,'Nicho', 'be humble!','Obama',20,10,new Date(2021,4,15)),
     new Quote(3,'Jackob', 'wake up!','Obama',20,10,new Date(2021,4,15)),
      new Quote(4,'Maalim', 'you are good!','Obama',20,10,new Date(2021,4,15)),
@@ -16,26 +16,26 @@ export class QuoteComponent implements OnInit {
     
   ];
 
-  addNewQuote(quote:any){
+  get sortQuotes() {
+    return this.quotes.sort((a, b) => {
+      return <any>new Date(b.completeDate) - <any>new Date(a.completeDate);
+    });
+  }  
+  addedQuote(quote:any){
     let arraysize = this.quotes.length;
-    
     quote.id = arraysize+1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
-
-toggleDetails(index:any){
-  this.quotes[index].showDescription = !this.quotes[index].showDescription;
-}
-deleteQuote(isComplete:any, index:any){
-  if (isComplete) {
-    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
-
-    if (toDelete){
-      this.quotes.splice(index,1)
+  quoteDelete(isRead:any, index:any){
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if(toDelete){
+        this.quotes.splice(index,1);
+      }
+      
     }
   }
-}
 
 
   constructor() { }
